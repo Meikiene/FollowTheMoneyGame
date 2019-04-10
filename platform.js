fc4628466e897470357cde639c2b12df20d422fb
Meikiene <42394095+Meikiene@users.noreply.github.com>
2019-04-09 19:28:06 -1000

    second commit

--- platform.js
+++ platform.js
@@ -1,8 +1,8 @@
 function platform(){
   this.x = 0;
-  this.y = h-20;
+  this.y = h-35;
   this.width = w;
-  this.height = h-20;
+  this.height = h-35;
   this.show = function(){
     fill(color('green'));
     rect(this.x,this.y,this.width,this.height);
